e4a8db709e6e9794272bb2d52d13fbc8c4afd40b
Megan Sullivan <3609989+meganesu@users.noreply.github.com>
2024-09-27 23:47:25 -0700

    fix: add tbody to table; remove console.logs

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,37 +1,35 @@
 import "./Board.css"
 
 const Board = ({ updateDialog, restaurantList }) => {
-  console.log(restaurantList)
-
   const restaurantList2D = []
   for (let i = 0; i < restaurantList.length; i += 5) {
     const row = restaurantList.slice(i, i + 5)
     restaurantList2D.push(row)
   }
-  console.log(restaurantList2D)
-
 
   return (
     <table>
-      {
-        restaurantList2D.map(row => {
-          return (
-            <tr>
-              { row.map(restaurant => (
-                <td>
-                  <button
-                    className={restaurant.visited && "visited"}
-                    onClick={() => {updateDialog(restaurant)}}
-                    aria-label={`${restaurant.restaurantName}, ${restaurant.visited ? 'visited' : 'not visited'}`}
-                  >
-                    {restaurant.restaurantName}
-                  </button>
-                </td>
-              )) }
-            </tr>
-          )
-        })
-      }
+      <tbody>
+        {
+          restaurantList2D.map(row => {
+            return (
+              <tr>
+                { row.map(restaurant => (
+                  <td>
+                    <button
+                      className={restaurant.visited && "visited"}
+                      onClick={() => {updateDialog(restaurant)}}
+                      aria-label={`${restaurant.restaurantName}, ${restaurant.visited ? 'visited' : 'not visited'}`}
+                    >
+                      {restaurant.restaurantName}
+                    </button>
+                  </td>
+                )) }
+              </tr>
+            )
+          })
+        }
+      </tbody>
     </table>
   )
 }
